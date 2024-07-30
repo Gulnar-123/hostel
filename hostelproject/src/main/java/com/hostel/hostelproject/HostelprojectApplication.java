4841efa44f22f0df2334251a917a211f7a5b5f76
Gulnar-123 <gulnar6816@gmail.com>
2024-07-30 23:06:44 +0530

    First commit

---
+++ hostelproject/src/main/java/com/hostel/hostelproject/HostelprojectApplication.java
@@ -0,0 +1,13 @@
+package com.hostel.hostelproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HostelprojectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HostelprojectApplication.class, args);
+	}
+
+}
---
+++ hostelproject/src/test/java/com/hostel/hostelproject/HostelprojectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hostel.hostelproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HostelprojectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
